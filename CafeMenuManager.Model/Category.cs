1acca33152b2b3aa25935197bb7cf18e8fcf9d31
Prabhjot <prabhjotk0187@gmail.com>
2026-08-10 22:35:50 -0500

    Add three domain entities for cafe menu manager

---
+++ CafeMenuManager.Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeMenuManager.Model
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<MenuItem> MenuItems { get; set; }
+    }
+}
---
+++ CafeMenuManager.Model/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeMenuManager.Model
+{
+    public class MenuItem
+    {
+        public int MenuItemId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
