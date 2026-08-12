751a154c2da545cee52a02f94e58bdbc23ba46e4
Prabhjot <prabhjotk0187@gmail.com>
2026-08-12 09:58:29 -0500

    add the menuitem service method the purpose add the items , update it , get the items , if wnats delete it

--- CafeMenuManager.BLL/MenuItemService.cs
+++ CafeMenuManager.BLL/MenuItemService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CafeMenuManager.DAL;
+using CafeMenuManager.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,52 @@ using System.Threading.Tasks;
 
 namespace CafeMenuManager.BLL
 {
-    internal class MenuItemService
+    public class MenuItemService
     {
+        private readonly CafeMenuContext _context;
+
+        public MenuItemService(CafeMenuContext context)
+        {
+            _context = context;
+        }
+
+        public List<MenuItem> GetAll()
+        {
+            return _context.MenuItems
+                .Include(m => m.Category)
+                .Include(m => m.Ingredients)
+                .ToList();
+        }
+
+        public MenuItem GetById(int id)
+        {
+            return _context.MenuItems
+                .Include(m => m.Category)
+                .Include(m => m.Ingredients)
+                .FirstOrDefault(m => m.MenuItemId == id);
+        }
+
+        public void Add(MenuItem menuItem)
+        {
+            _context.MenuItems.Add(menuItem);
+            _context.SaveChanges();
+        }
+
+        public void Update(MenuItem menuItem)
+        {
+            _context.MenuItems.Update(menuItem);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            MenuItem menuItem = _context.MenuItems.Find(id);
+
+            if (menuItem != null)
+            {
+                _context.MenuItems.Remove(menuItem);
+                _context.SaveChanges();
+            }
+        }
     }
 }
