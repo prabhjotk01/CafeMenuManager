2806e3f35c46c6d5282399fa6f85c5a830d8eca7
Prabhjot <prabhjotk0187@gmail.com>
2026-08-12 22:19:20 -0500

    Add CategoryService with CRUD operations

--- CafeMenuManager.BLL/CategoryService.cs
+++ CafeMenuManager.BLL/CategoryService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CafeMenuManager.DAL;
+using CafeMenuManager.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,50 @@ using System.Threading.Tasks;
 
 namespace CafeMenuManager.BLL
 {
-    internal class CategoryService
+    public  class CategoryService
     {
+        private readonly CafeMenuContext _context;
+
+        public CategoryService(CafeMenuContext context)
+        {
+            _context = context;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _context.Categories
+                .Include(c => c.MenuItems)
+                .ToList();
+        }
+
+        public Category GetById(int id)
+        {
+            return _context.Categories
+                .Include(c => c.MenuItems)
+                .FirstOrDefault(c => c.CategoryId == id);
+        }
+
+        public void Add(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Category category = _context.Categories.Find(id);
+
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
     }
 }
